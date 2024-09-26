dffe52123448662c97f25a0f43662b0d26dc90a5
Tahani-mohammed <Tahaniii.moh@gmail.com>
2024-09-26 11:47:41 +0300

    Initial Commit

---
+++ Tahaninew5/Tahaninew5App.swift
@@ -0,0 +1,17 @@
+//
+//  Tahaninew5App.swift
+//  Tahaninew5
+//
+//  Created by Tahani Mohammed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tahaninew5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tahaninew5Tests/Tahaninew5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tahaninew5Tests.swift
+//  Tahaninew5Tests
+//
+//  Created by Tahani Mohammed on 23/03/1446 AH.
+//
+
+import Testing
+@testable import Tahaninew5
+
+struct Tahaninew5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
